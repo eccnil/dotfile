eccb0387c135a5e0cf0050c37a56b5881b76c38c
eccnil <eccnil@gmail.com>
2023-07-07 22:18:31 +0200

    chemacs stow

---
+++ chemacs2/.emacs-profiles.el
@@ -0,0 +1,3 @@
+(("default"   . ((user-emacs-directory . "~/.emacs_profiles/doom")))
+ ("doom"   . ((user-emacs-directory . "~/.emacs_profiles/doom-emacs")))
+ ("spacemacs"   . ((user-emacs-directory . "~/.emacs_profiles/spacemacs"))))
